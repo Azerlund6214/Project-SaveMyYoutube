b4695bca9f5428ffc2ca07d7d47a0e8dabf1b742
Azerlund6214 <67274297+Azerlund6214@users.noreply.github.com>
2020-07-27 17:05:18 +0300

    Убрал не нужное в отдельную папку

--- Get_Table.php
+++ Get_Table.php
@@ -4,25 +4,25 @@
 	###########################################
 	###################### Подготовка
 	
-	require_once "PRINTER=01=240320=SF_PRINTER.php";
-	//require_once "0=MAIN=MAIN_DEFINER.php";
-	//require_once "1=MAIN=INCLUDER.php";
-	//require_once "2=MAIN=INI_SETTER.php";
-	//require_once "3=MAIN=EXTENSION_CHECKER.php";
 	
-	//require_once "99=PROJECT_DEFINES.php";
-	
-	# По-хорошему убрать бы их в "1=MAIN=INCLUDER.php"
-	//$UN_MYSQLC = new UNIV_PHP_MYSQL_Controller();
-	//$UN_SMF    = new UNIV_PHP_Small_Func();
-	//$UN_TIMER  = new UNIV_PHP_TIMER();
-	//$UN_WEB    = new UNIV_PHP_WEB();
-	#$UN_SRVCONST = new UNIV_PHP_SERVER_CONST();
 	
 	
 	require_once "UNIV_PHP_CLASS_YOUTUBE.php";
 	$UN_YOUTUBE = new UNIV_PHP_YOUTUBE();
 	
+	
+	require_once "SF_CLASS.php";
+	//SF::PRINTER(Array("123","12"));
+	//SF::Print_Class_Func_and_Vars($UN_YOUTUBE);
+	//SF::Memory_Usage_EchoGet("M","Echo");
+	//SF::Echo_This_File_Path();
+	//echo SF::Get_This_Server_Domain(true,true,"213");
+	//SF::PRINTER( SF::Get_Server_Headers() );
+	//SF::PRINTER( SF::Get_HTTP_Response() );
+	
+	
+	
+	//exit;
 	
 	
 	#echo "<hr>".$UN_WEB->Get_HTTP_Response("https://www.youtube.com/watch?v=1");
@@ -45,7 +45,7 @@
 	
 
 	if( isset($_POST["cb1_Echo_POST"]) )
-		SF_PRINTER($_POST , "ВСЕ пришедшее в POST");
+		SF::PRINTER($_POST , "ВСЕ пришедшее в POST");
 	
 	######################################################################################
 	###########################################
@@ -54,19 +54,19 @@
 	
 	$UN_YOUTUBE -> Set_URL( $user_request_url );
 	if( isset($_POST["cb2_Echo_URL"]) )
-		SF_PRINTER($UN_YOUTUBE->Full_Target_Video_URL , "Приняли URL");
+		SF::PRINTER($UN_YOUTUBE->Full_Target_Video_URL , "Приняли URL");
 	
 	######################
 	
 	$UN_YOUTUBE -> Prepare_URL(  );
 	if( isset($_POST["cb3_Echo_URL_Prep"]) )
-		SF_PRINTER($UN_YOUTUBE->Target_Video_URL , "Подготовленный URL(Без пробелов)");
+		SF::PRINTER($UN_YOUTUBE->Target_Video_URL , "Подготовленный URL(Без пробелов)");
 	
 	######################
 	
 	$UN_YOUTUBE -> Get_Video_ID(  );
 	if( isset($_POST["cb4_Echo_Video_ID"]) )
-		SF_PRINTER($UN_YOUTUBE->Video_ID , "Итоговый Video_ID");
+		SF::PRINTER($UN_YOUTUBE->Video_ID , "Итоговый Video_ID");
 	
 	######################
 	############################################
@@ -76,30 +76,30 @@
 	$UN_YOUTUBE -> Decode_Video_Info(  );
 	
 	if( isset($_POST["cb5_Echo_Video_Info_Decoded"]) )
-		SF_PRINTER($UN_YOUTUBE->Video_Info , "Video_Info Декодированный 2 раза, одной строкой");
+		SF::PRINTER($UN_YOUTUBE->Video_Info , "Video_Info Декодированный 2 раза, одной строкой");
 	
 	
 	$UN_YOUTUBE -> Convert_Video_Info_To_Asoc(  );
 	if( isset($_POST["cb5_Echo_Video_Info_Asoc"]) )
-		SF_PRINTER($UN_YOUTUBE->Video_Info_Asoc , "Video_Info_Asoc ВЕСЬ;  COUNT=".count($UN_YOUTUBE->Video_Info_Asoc));
+		SF::PRINTER($UN_YOUTUBE->Video_Info_Asoc , "Video_Info_Asoc ВЕСЬ;  COUNT=".count($UN_YOUTUBE->Video_Info_Asoc));
 	
 	######################
 	
 	$UN_YOUTUBE -> Check_Video_Response_Status(  );  # Дальше  либо exit в ERROR_HANDLER
 	if( isset($_POST["cb6_Echo_Video_Status_Is_OK"]) )
-		SF_PRINTER($UN_YOUTUBE->Video_Status_Is_OK , "Video_Status_Is_OK = Видео можно смотреть");
+		SF::PRINTER($UN_YOUTUBE->Video_Status_Is_OK , "Video_Status_Is_OK = Видео можно смотреть");
 	
 	######################
 	
 	$UN_YOUTUBE -> Player_Response_Convert_JSON_To_Asoc(  );  #
 	if( isset($_POST["cb7_Echo_JSON_Full"]) )
-		SF_PRINTER($UN_YOUTUBE->Player_Response_JSON_Full , "Player_Response_JSON_Full = Полный JSON");
+		SF::PRINTER($UN_YOUTUBE->Player_Response_JSON_Full , "Player_Response_JSON_Full = Полный JSON");
 	
 	
 	#  ВЫРЕЗАТЬ!!!
 	$UN_YOUTUBE -> Player_Response_JSON_Erase(  );  #
 	if( isset($_POST["cb8_Echo_JSON_Erased"]) )
-		SF_PRINTER($UN_YOUTUBE->Player_Response_JSON_Erased , "Player_Response_JSON_Erased = Урезанный JSON");
+		SF::PRINTER($UN_YOUTUBE->Player_Response_JSON_Erased , "Player_Response_JSON_Erased = Урезанный JSON");
 	
 	
 	######################
@@ -110,31 +110,31 @@
 	
 	$UN_YOUTUBE -> Fill_FIN_Video_Info(  );  #
 	if( isset($_POST["cb9_Echo_FIN_Video_Info_Asoc"]) )
-		SF_PRINTER($UN_YOUTUBE->FIN_Video_Info_Asoc , "FIN_Video_Info_Asoc = ВСЯ Основная инфа о видео");
+		SF::PRINTER($UN_YOUTUBE->FIN_Video_Info_Asoc , "FIN_Video_Info_Asoc = ВСЯ Основная инфа о видео");
 	
 	######################
 	
 	$UN_YOUTUBE -> Fill_FIN_Video_Thimbnails_Url_Arr(  );
 	if( isset($_POST["cb10_Echo_FIN_Video_Thumb_Url_Arr"]) )
-		SF_PRINTER($UN_YOUTUBE->FIN_Video_Thimbnails_Url_Arr , "FIN_Video_Thimbnails_Url_Arr = Все Thimbnails");
+		SF::PRINTER($UN_YOUTUBE->FIN_Video_Thimbnails_Url_Arr , "FIN_Video_Thimbnails_Url_Arr = Все Thimbnails");
 	
 	######################
 	
 	$UN_YOUTUBE -> Fill_FIN_Video_Itag_Info_Asoc_FULL(  );
 	if( isset($_POST["cb11_Echo_FIN_Video_Itag_Info_Asoc_FULL"]) )
-		SF_PRINTER($UN_YOUTUBE->FIN_Video_Itag_Info_Asoc_FULL , "FIN_Video_Itag_Info_Asoc_FULL = ВСЯ Инфа о форматах видео");
+		SF::PRINTER($UN_YOUTUBE->FIN_Video_Itag_Info_Asoc_FULL , "FIN_Video_Itag_Info_Asoc_FULL = ВСЯ Инфа о форматах видео");
 	
 	######################
 	
 	$UN_YOUTUBE -> Fill_FIN_Video_Itag_Info_Asoc_Video_MP4(  );
 	
 	if( isset($_POST["cb12_Echo_FIN_Video_Itag_Info_Asoc_Video_MP4"]) )
-		SF_PRINTER($UN_YOUTUBE->FIN_Video_Itag_Info_Asoc_Video_MP4 , "FIN_Video_Itag_Info_Asoc_Video_MP4 = ВСЯ Инфа о форматах видео(Только MP4)");
+		SF::PRINTER($UN_YOUTUBE->FIN_Video_Itag_Info_Asoc_Video_MP4 , "FIN_Video_Itag_Info_Asoc_Video_MP4 = ВСЯ Инфа о форматах видео(Только MP4)");
 	
 	
 	$UN_YOUTUBE -> Fill_FIN_Video_Itag_Info_Asoc_Audio(  );
 	if( isset($_POST["cb13_Echo_FIN_Video_Itag_Info_Asoc_Audio"]) )
-		SF_PRINTER($UN_YOUTUBE->FIN_Video_Itag_Info_Asoc_Audio , "FIN_Video_Itag_Info_Asoc_Audio = ВСЯ Инфа о форматах AUDIO");
+		SF::PRINTER($UN_YOUTUBE->FIN_Video_Itag_Info_Asoc_Audio , "FIN_Video_Itag_Info_Asoc_Audio = ВСЯ Инфа о форматах AUDIO");
 	
 	
 	######################
